274299d17701b79a5549519627581567646fdf4a
ashbrooke8 <ashleybhunchak@gmail.com>
2024-11-09 13:53:17 -0700

    more file structure

---
+++ default_results.js
@@ -0,0 +1,3 @@
+import { Result } from "./result.js";
+
+//THIS FILE IS TO POPULATE THE PAGE WITH THE RESULTS ELEMENTS
---
+++ result.js
@@ -0,0 +1,9 @@
+// THIS FILE IS TO GENERATE A RESULT TILE
+
+
+// RESULT CONSTRUCTOR
+function Result(details) {
+    //FILL IN WITH DETAILS
+}
+
+export {Result};
